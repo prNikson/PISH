4c645f683a54af7f1fec27f11709644bb27042c3
ivan-karaberov <karaberovivan123456@gmail.com>
2023-04-30 22:33:49 +0300

    Created Main Page

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 from config import Configuration
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../../frontend/html/')
 app.config.from_object(Configuration)
--- backend/app/view.py
+++ backend/app/view.py
@@ -1,5 +1,30 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def homepage():
-    return "Home Page"
+    return render_template("index.html")
+
+@app.route("/news")
+def news():
+   return "News"
+
+@app.route("/events")
+def events():
+    return "Events"
+
+@app.route("/organizations")
+def organizations():
+    return "Organizations"
+
+@app.route("/contacts")
+def contacts():
+    return "Contacts"
+
+@app.route("/auth")
+def auth():
+    return "auth"
+
+@app.route("/registration")
+def registation():
+    return "Registration"
